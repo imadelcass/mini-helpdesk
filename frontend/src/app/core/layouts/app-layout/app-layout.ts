93bc27ce199afec0d85957fbf17bc38a024e1d82
imadelcass <imadelcass@gmail.com>
2025-10-16 10:45:40 +0100

    feat(layout): update button classes for dynamic styling and enhance router imports

--- frontend/src/app/core/layouts/app-layout/app-layout.ts
+++ frontend/src/app/core/layouts/app-layout/app-layout.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { Auth } from '@core/services/auth';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { ButtonGroupModule } from 'primeng/buttongroup';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [RouterOutlet, AvatarModule, ButtonModule, ButtonGroupModule],
+  imports: [RouterOutlet, AvatarModule, ButtonModule, ButtonGroupModule, RouterLink, CommonModule],
   templateUrl: './app-layout.html',
 })
 export class AppLayout {
-  constructor(public authService: Auth) {}
+  constructor(public authService: Auth, public router: Router) {}
 
   currentYear: number = new Date().getFullYear();
 }
